3ccf342086f278ca2503ad012848daa42c0f5f64
mikershov94 <ershovme@gmail.com>
2021-07-21 21:46:45 +0800

    Created header of table

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -56,12 +56,30 @@ class App extends React.Component {
                 <tr className="table__row table__row_header-2">
                     <th className="table__cell table__cell_col-1">Название</th>
                     <th className="table__cell table__cell_col-2">
-                        <th className="table__cell table__cell_subcol-1">Показы</th>
+                        <th className="table__cell table__cell_subcol-1">
+                            <div>Показы</div>
+                            <span className="material-icons">help_outline</span>
+                        </th>
                         <th className="table__cell table__cell_subcol-2">Клики</th>
-                        <th className="table__cell table__cell_subcol-3">Сеансы</th>
-                        <th className="table__cell table__cell_subcol-4">CTR, %</th>
+                        <th className="table__cell table__cell_subcol-3">
+                            <div>Сеансы</div>
+                            <span className="material-icons">
+                                arrow_downward
+                            </span>
+                        </th>
+                        <th className="table__cell table__cell_subcol-4">
+                            <div>CTR, %</div>
+                            <span className="material-icons">help_outline</span>
+                        </th>
                         <th className="table__cell table__cell_subcol-5">Цена клика</th>
-                        <th className="table__cell table__cell_subcol-6">Затраты</th>
+                        <th className="table__cell table__cell_subcol-6">
+                            <div>Затраты</div>
+                            <div>
+                                <a>Настроить
+                                    <span className="material-icons">help_outline</span>
+                                </a>
+                            </div>
+                        </th>
                     </th>
                     <th className="table__cell table__cell_col-3">
                         <th className="table__cell table__cell_subcol-7">Кол-во</th>
@@ -77,22 +95,22 @@ class App extends React.Component {
                 <tr className="table__row table__row_header-3">
                     <th className="table__cell table__cell_col-1">Итого</th>
                     <td className="table__cell table__cell_col-2">
-                        <td className="table__cell table__cell_subcol-1">0</td>
-                        <td className="table__cell table__cell_subcol-2">0</td>
-                        <td className="table__cell table__cell_subcol-3">0</td>
-                        <td className="table__cell table__cell_subcol-4">0</td>
-                        <td className="table__cell table__cell_subcol-5">0</td>
-                        <td className="table__cell table__cell_subcol-6">0</td>
+                        <td className="table__cell table__cell_data table__cell_subcol-1">0</td>
+                        <td className="table__cell table__cell_data table__cell_subcol-2">0</td>
+                        <td className="table__cell table__cell_data table__cell_subcol-3">0</td>
+                        <td className="table__cell table__cell_data table__cell_subcol-4">0</td>
+                        <td className="table__cell table__cell_data table__cell_subcol-5">0</td>
+                        <td className="table__cell table__cell_data table__cell_subcol-6">0</td>
                     </td>
                     <td className="table__cell table__cell_col-3">
-                        <td className="table__cell table__cell_subcol-7">0</td>
-                        <td className="table__cell table__cell_subcol-8">0</td>
-                        <td className="table__cell table__cell_subcol-9">0</td>
+                        <td className="table__cell table__cell_data table__cell_subcol-7">0</td>
+                        <td className="table__cell table__cell_data table__cell_subcol-8">0</td>
+                        <td className="table__cell table__cell_data table__cell_subcol-9">0</td>
                     </td>
                     <td className="table__cell table__cell_col-4">
-                        <td className="table__cell table__cell_subcol-10">0</td>
-                        <td className="table__cell table__cell_subcol-11">0</td>
-                        <td className="table__cell table__cell_subcol-12">0</td>
+                        <td className="table__cell table__cell_data table__cell_subcol-10">0</td>
+                        <td className="table__cell table__cell_data table__cell_subcol-11">0</td>
+                        <td className="table__cell table__cell_data table__cell_subcol-12">0</td>
                     </td>
                 </tr>
             </table>
