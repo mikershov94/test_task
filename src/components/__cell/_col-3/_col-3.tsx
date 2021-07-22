2894e46fd4e48543371725cc9c5cbac7ca29f0cc
mikershov94 <ershovme@gmail.com>
2021-07-22 14:18:32 +0800

    Added component _col-3

---
+++ src/components/__cell/_col-3/_col-3.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./_col-3.sass";
+
+const CellCol3 = () => {
+    return(
+        <th className="table__cell table__cell_col-3"></th>
+    );
+}
+
+export default CellCol3;
---
+++ src/components/__cell/_col-3/index.ts
@@ -0,0 +1,2 @@
+import CellCol3 from './_col-3';
+export default CellCol3;
