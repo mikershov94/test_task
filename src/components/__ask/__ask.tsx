fd9c7096a05eb27fe2148258c22ee23048306d75
mikershov94 <ershovme@gmail.com>
2021-07-22 13:03:24 +0800

    Added component __ask

---
+++ src/components/__ask/__ask.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./__ask.sass";
+
+const Ask = () => {
+    return(
+        <span className="table__ask material-icons">
+            help
+        </span>
+    );
+}
+
+export default Ask;
---
+++ src/components/__ask/index.ts
@@ -0,0 +1,2 @@
+import Ask from "./__ask";
+export default Ask;
