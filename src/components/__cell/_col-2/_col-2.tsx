2ffcc8aeb3454bf5172c49840a67491addafc963
mikershov94 <ershovme@gmail.com>
2021-07-22 14:15:34 +0800

    Added component _col-2

---
+++ src/components/__cell/_col-2/_col-2.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./_col-2.sass";
+
+const CellCol2 = () => {
+    return(
+        <th className="table__cell table__cell_col-2">Трафик</th>
+    );
+}
+
+export default CellCol2;
---
+++ src/components/__cell/_col-2/index.ts
@@ -0,0 +1,2 @@
+import CellCol2 from './_col-2';
+export default CellCol2;
